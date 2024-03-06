38ebd3ae2c4d4c2140107c0d2401443adac57025
chaimaadln <160984893+chaimaadln@users.noreply.github.com>
2024-03-06 11:57:38 +0100

    Update test_streamlet.py

--- test_streamlet.py
+++ test_streamlet.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 # Create a title for the app
 st.title("My Streamlit App")
+st.write("hello worlfd]")
 
 # Add a text input field
 user_input = st.text_input("Enter some text")
